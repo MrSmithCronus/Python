935c7984f9a5418a71f378f6e226a6008dc630b7
MrSmithCronus <41115987+MrSmithCronus@users.noreply.github.com>
2018-07-28 12:49:53 +0400

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -1,11 +1,18 @@
 # A simple program illustrating chaotic behaviour
+import random
+
 
 def main():
-    print ("This program illustrates a chaotic function")
+    print("This program illustrates a chaotic function")
     x = float((input("Enter a number between 0 and 1: ")))
-    for i in range(10):
-        x = 3.9 * x * (1-x)
-        print (x)
+    if 0 < x < 1:
+        for i in range(10):
+            x = random.randint(0, 10) * x * (1-x)
+            print(x)
+    else:
+        print("Please enter a number BETWEEN 0 and 1")
+
 
 if __name__ == '__main__':
     main()
+
